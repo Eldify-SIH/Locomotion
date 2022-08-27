9f18a89011a15ebb4c381ac0f81a98ec3c22cca0
Tanvie <tanviee09@gmail.com>
2022-08-28 04:53:06 +0530

    right left forward

--- tanvi/tanvi.ino
+++ tanvi/tanvi.ino
@@ -1,9 +1,150 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <ESP32Servo.h>
+#define front1 17
+#define front2 16
+#define front3 32
+#define front4 33
+
+#define back5 25
+#define back6 26
+#define back7 27
+#define back8 14
+
+Servo Servofront1;
+Servo Servofront2;
+Servo Servofront3;
+Servo Servofront4;
+
+Servo Servoback5;
+Servo Servoback6;
+Servo Servoback7;
+Servo Servoback8;
+
+int min1, max1;
+int min2, max2;
+int min3, max3;
+int min4, max4;
+int min5, max5;
+int min6, max6;
+int min7, max7;
+int min8, max8;
+void setup()
+{
+  Serial.begin(9600);
+  Servofront1.attach(front1);
+  Servofront2.attach(front2);
+  Servofront3.attach(front3);
+  Servofront4.attach(front4);
+
+  Servoback5.attach(back5);
+  Servoback6.attach(back6);
+  Servoback7.attach(back7);
+  Servoback8.attach(back8);
+
+
+  Serial.begin(9600);
+  Servofront1.write(80);
+  Servofront2.write(140);
+  Servofront3.write(110);
+  Servofront4.write(40);
+
+  Servoback5.write(40);
+  Servoback6.write(150);
+  Servoback7.write(125);
+  Servoback8.write(40);
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+
+void loop()
+{
+ 
+}
+
+void forward() {
+  Servofront2.write(165);
+  Servoback8.write(15);
+  delay(70);
+  Servofront1.write(100);
+  Servoback7.write(115);
+  delay(70);
+  Servofront2.write(140);
+  Servoback8.write(40);
+  delay(70);
+  Servoback5.write(30);
+  delay(70);
+  Servofront3.write(120);
+  delay(70);
+  Servofront4.write(15);
+  Servoback6.write(175);
+  delay(70);
+  Servofront3.write(110);
+  Servoback5.write(40);
+  delay(70);
+  Servofront4.write(40);
+  Servoback6.write(150);
+  delay(70);
+  Servofront1.write(80);
+  Servoback7.write(125);
+  //    delay(500);
+  delay(200);
+}
+
+void left() {
+  Servofront2.write(165);
+  //  Servoback8.write(15);
+  delay(70);
+  Servofront1.write(100);
+  Servoback7.write(115);
+  delay(70);
+  Servofront2.write(140);
+  //  Servoback8.write(40);
+  delay(70);
+  Servoback5.write(30);
+  delay(70);
+  Servofront3.write(120);
+  delay(70);
+  Servofront4.write(15);
+  Servoback6.write(175);
+  delay(70);
+  Servofront3.write(110);
+  Servoback5.write(40);
+  delay(70);
+  Servofront4.write(40);
+  Servoback6.write(150);
+  delay(70);
+  Servofront1.write(80);
+  Servoback7.write(125);
+  //    delay(500);
+  delay(200);
+}
+
+void right(){
+  
+  Servofront2.write(145);
+  Servoback8.write(15);
+  delay(70);
+  Servofront1.write(90);
+  Servoback7.write(115);
+  delay(70);
+  Servofront2.write(140);
+  Servoback8.write(40);
+  delay(70);
+  Servoback5.write(30);
+  delay(70);
+  Servofront3.write(120);
+  delay(70);
+  Servofront4.write(15);
+  //  Servoback6.write(175);
+  delay(70);
+  Servofront3.write(110);
+  Servoback5.write(40);
+  delay(70);
+  Servofront4.write(40);
+  //  Servoback6.write(150);
+  delay(70);
+  Servofront1.write(85);
+  Servoback7.write(125);
+  //    delay(500);
+  delay(200);
 
 }
